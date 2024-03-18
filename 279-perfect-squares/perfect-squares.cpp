5cae761ef26e96d1b7c6c58ad006cba76439f634
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-03-18 21:26:30 +0530

    Time: 61 ms (44.74%) | Memory: 10.6 MB (47.92%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &dp){
+    int solveMem(int n, vector<int> &dp){
 	   if(n == 0)   return 0;
 	   
 	   if(dp[n] != -1)  return dp[n];
@@ -8,14 +8,33 @@ public:
 	   int ans = n;
 	   for(int i=1;i*i<=n;i++){
 	       int temp = i * i;
-	       ans = min( ans, 1 + solve(n-temp, dp) );
+	       ans = min( ans, 1 + solveMem(n-temp, dp) );
 	   }
 	   dp[n] = ans;
 	   return dp[n];
 	}
 
+    int solveTab(int n){
+	   vector<int> dp(n+1, INT_MAX);
+	   dp[0] = 0;
+	   
+	   for(int i=1;i<=n;i++){
+	       for(int j=1;j*j<=n;j++){
+    	       int temp = j * j;
+    	       if(i - temp >= 0){
+    	          dp[i] = min( dp[i], 1 + dp[i - temp] );
+    	       }
+	       }
+	   }
+	   return dp[n];
+	}
+
     int numSquares(int n) {
-        vector<int> dp(n+1, -1);
-        return solve(n, dp);
+        //Approach 1
+        // vector<int> dp(n+1, -1);
+        // return solveMem(n, dp);
+
+        //Approach 2
+        return solveTab(n);
     }
 };

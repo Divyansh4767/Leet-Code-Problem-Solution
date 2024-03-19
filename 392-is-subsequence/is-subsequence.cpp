af27f17a904fed03c96d86c82915395b15e3fe99
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-03-19 15:39:14 +0530

    Time: 296 ms (13.07%) | Memory: 525.9 MB (5.20%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(int i, int j, string s, string t, vector<vector<int>> &dp){
+        //base case
+        if(i < 0)   return 1;
+        if(j < 0)   return 0;
+        if(dp[i][j] != -1)  return dp[i][j];
+
+        if(s[i] == t[j]){
+            dp[i][j] = solve(i-1, j-1, s, t, dp) + solve(i, j-1, s, t, dp);
+        }
+        else{
+            dp[i][j] = solve(i, j-1, s, t, dp);
+        }
+        return dp[i][j];
+    }
+    bool isSubsequence(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return solve(n-1, m-1, s, t, dp);
+    }
+};

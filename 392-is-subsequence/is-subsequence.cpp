61d4bb1d7da1fee4d97270287de89b741ef75694
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-03-19 15:39:27 +0530

    Time: 288 ms (13.07%) | Memory: 526 MB (5.20%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -7,10 +7,10 @@ public:
         if(dp[i][j] != -1)  return dp[i][j];
 
         if(s[i] == t[j]){
-            dp[i][j] = solve(i-1, j-1, s, t, dp) + solve(i, j-1, s, t, dp);
+            return dp[i][j] = solve(i-1, j-1, s, t, dp) + solve(i, j-1, s, t, dp);
         }
         else{
-            dp[i][j] = solve(i, j-1, s, t, dp);
+            return dp[i][j] = solve(i, j-1, s, t, dp);
         }
         return dp[i][j];
     }

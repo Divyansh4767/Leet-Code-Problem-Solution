2bfc243b5a08e82074db66638aa93ed84febf27b
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-05 19:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (51.96%) - LeetSync

--- 500-keyboard-row/keyboard-row.cpp
+++ 500-keyboard-row/keyboard-row.cpp
@@ -2,18 +2,17 @@
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
-        string s1 = "qwertyuiop";
-        string s2 = "asdfghjkl";
-        string s3 = "zxcvbnm";
+        string s1 = "qwertyuiop", s2 = "asdfghjkl", s3 = "zxcvbnm";
 
         unordered_map<char,int> mp;
         vector<string> ans;
 
-        for(auto it:s1){ mp[it]=1; } //mp(value:key): {'a': 1, 'b': 1}
-        for(auto it:s2){ mp[it]=2; }
-        for(auto it:s3){ mp[it]=3; }
+        //mp(value:key): {'a': 1, 's': 1, 'd': 1, 'f': 1.....}
+        for(auto &it:s1){ mp[it]=1; }
+        for(auto &it:s2){ mp[it]=2; }
+        for(auto &it:s3){ mp[it]=3; }
 
-        for(auto it: words){
+        for(auto &it: words){
             int j;
             for(j=1;j<it.size();j++){
                 if(mp[tolower(it[j])] != mp[tolower(it[j-1])]){

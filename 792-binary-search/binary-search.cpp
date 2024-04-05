6dbf138d498060c15b52036ff22b1df1d65159e7
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-05 15:51:51 +0530

    Time: 32 ms (21.32%) | Memory: 29.8 MB (67.18%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size()-1;
+        int left = 0, right = nums.size()-1, mid;
         while(left <= right){
-            int mid = left + (right-left)/2;
+            mid = left + (right-left)/2;
             if(nums[mid] == target){
-                return mid;
+                break;
             }
             else if(nums[mid] > target){
                 right = mid - 1;
@@ -14,6 +14,6 @@ public:
                 left = mid + 1;
             }
         }
-        return -1;
+        return nums[mid] == target ? mid : -1;
     }
 };

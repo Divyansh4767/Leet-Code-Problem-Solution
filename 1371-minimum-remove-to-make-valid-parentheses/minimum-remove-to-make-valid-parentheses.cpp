1af657e0fdfdd097095a01e03ec6be66a814d78a
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-06 16:57:46 +0530

    Time: 22 ms (50.77%) | Memory: 11 MB (95.54%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -4,15 +4,14 @@ public:
     string minRemoveToMakeValid(string s) {
         stack<char> st;
         for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(') {
-                st.push(s[i]);
-            }
-            else if (s[i] == ')') {
-                if(!st.empty() && st.top() == '(') {
+            if (s[i] == ')') {
+                if (!st.empty() && st.top() == '(') {
                     st.pop();
                 } else if (st.empty()){
                     s[i] = '.';
                 }
+            } else if (s[i] == '(') {
+                st.push(s[i]);
             }
         }
 

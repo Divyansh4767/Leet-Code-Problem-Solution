9501a157bcc572d910abe462f57e279dd87c68ab
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-08 18:54:31 +0530

    Time: 3 ms (26.11%) | Memory: 7 MB (56.28%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -4,7 +4,7 @@ public:
         if(num == 1)    return true;
         int low = 1, high = num/2;
         while(low <= high){
-            long long int mid = low + (high - low) / 2;
+            long long mid = low + (high - low) / 2;
             if(mid*mid == num){
                 return true;
             }

7b7ad118670af2b3c25c0840bacf7b6265459bbc
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-09 17:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (52.04%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x){
+            if(ans > INT_MAX/10 || ans < INT_MIN/10)    return 0;
+            ans = ans*10 + x%10;
+            x = x/10;
+        }
+        return ans;
+    }
+};

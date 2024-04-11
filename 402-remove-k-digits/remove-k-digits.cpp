58125b68d388582562172378c0e38ee176bd3c32
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-11 15:09:49 +0530

    Time: 8 ms (80.81%) | Memory: 10.3 MB (26.88%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
+    string removeKdigits(string num, int k) {   //TC & SC - O(N)
         stack<char> st;
         //Remove larger digits before smaller ones
         for(int i=0;i<num.size();i++){
@@ -10,20 +10,17 @@ public:
             }
             st.push(num[i]);
         }
-
         //edge case - If it's a monotonic stack(always increasing) -> Remove last k digits
         while(k > 0){
             st.pop();
             k--;
         }   
-
         //Convert it into a string
         string ans = "";
         while(!st.empty()){
             ans += st.top();
             st.pop();
         }
-
         //edge case - to eliminate leading 0's like 0200
         int endIndex = 0;
         for(int i=ans.size()-1;i>=0;i--){
@@ -35,7 +32,6 @@ public:
         //Take string before leading 0's only
         ans = ans.substr(0, endIndex + 1);
         reverse(ans.begin(), ans.end());
-        
         if(ans == "")   ans = "0";
         return ans;
     }

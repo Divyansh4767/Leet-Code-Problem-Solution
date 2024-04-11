8479ed7a436b0362e5da1571f3bfd2ae65508640
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-11 20:11:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (78.46%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -3,39 +3,29 @@ public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> matrix(n,vector<int>(n));
         int left = 0, top = 0, right = n-1, down = n-1;
-        int count = 1, num = n*n;
+        int count = 1;
 
-        while(count <= num){
-            while(top <= down && left <= right){
-                //Right
-                for(int i=left;i<=right;i++){
-                    matrix[top][i] = count;
-                    count++;
-                }
-                top++;
-                //Down
-                for(int i=top;i<=down;i++){
-                    matrix[i][right] = count;
-                    count++;
-                }
-                right--;
-                //Left
-                if(top <= down){
-                    for(int i=right;i>=left;i--){
-                        matrix[down][i] = count;
-                        count++;
-                    }
-                    down--;
-                }
-                //Up
-                if(left <= right){
-                    for(int i=down;i>=top;i--){
-                        matrix[i][left] = count;
-                        count++;
-                    }
-                    left++;
-                }
+        while(count <= n*n){
+            //Right
+            for(int i=left;i<=right;i++,count++){
+                matrix[top][i] = count;
             }
+            top++;
+            //Down
+            for(int i=top;i<=down;i++,count++){
+                matrix[i][right] = count;
+            }
+            right--;
+            //Left
+            for(int i=right;i>=left;i--,count++){
+                matrix[down][i] = count;
+            }
+            down--;
+            //Up
+            for(int i=down;i>=top;i--,count++){
+                matrix[i][left] = count;
+            }
+            left++;
         }
         return matrix;
     }

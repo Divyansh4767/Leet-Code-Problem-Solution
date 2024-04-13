a54e5fbea0fa00d32335cc0f02521ec43b005b3c
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-13 17:39:41 +0530

    Time: 7 ms (41.76%) | Memory: 10.9 MB (80.43%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
-        st.push(-1);
-        vector<int> ans(nums2.size());
+        stack<int> st;  st.push(-1);
+        vector<int> ans(nums2.size()), res(nums1.size(), -1);
+
         for(int i=nums2.size()-1;i>=0;i--){
-            while(st.top() != -1 && nums2[i] > st.top()){
-                st.pop();
-            }
+            while(st.top() != -1 && nums2[i] > st.top()){   st.pop();}
+
             ans[i] = st.top();
             st.push(nums2[i]);
         }
-        vector<int> res(nums1.size(), -1);
         for(int i=0;i<nums1.size();i++){
-            int j =0;
-            while(nums1[i] != nums2[j]){
-                j++;
-            }
+            int j = 0;
+            while(nums1[i] != nums2[j]){    j++;}
+
             res[i] = ans[j];
         }
         return res;

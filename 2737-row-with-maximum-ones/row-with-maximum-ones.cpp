864ce750a5d7731c5a17c416c279a53430b9d2fc
DIVYANSH BINANI <81380298+Divyansh4767@users.noreply.github.com>
2024-04-16 22:12:50 +0530

    Time: 106 ms (48.49%) | Memory: 65 MB (23.28%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int lowerBound(vector<int> arr, int n, int x) {
-        int low = 0, high = n-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(arr[mid] >= x){
-                high = mid - 1;
-            }
-            else{
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
-
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
-        int index = 0, maxCount = 0;
+        int maxOne = 0, index = 0;
 
         for(int i=0;i<mat.size();i++){
-            sort(mat[i].begin(),mat[i].end());
-        }
-        for(int i=0;i<mat.size();i++){
-            int countOne = mat[0].size() - lowerBound(mat[i], mat[0].size(), 1);
-            if(countOne > maxCount){
-                maxCount = countOne;
+            int countOne=0;
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]==1){
+                    countOne++;
+                }
+            }
+            if(countOne > maxOne){
                 index = i;
+                maxOne = countOne;
             }
         }
-        return {index, maxCount};
+        return {index, maxOne};
     }
 };
